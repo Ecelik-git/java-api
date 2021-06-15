6ef37f9759da8cc4542b6d404453c7e7386bb06d
Ecelik-git <elvanchelik@gmail.com>
2021-06-15 18:56:17 -0500

    june15

---
+++ src/test/java/com/techproed2/javaapi01/AppTest.java
@@ -0,0 +1,13 @@
+package com.techproed2.javaapi01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
---
+++ src/test/java/com/techproed2/javaapi01/GetRequest02.java
@@ -0,0 +1,85 @@
+package com.techproed2.javaapi01;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.testng.asserts.SoftAssert;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+public class GetRequest02 {
+	/*
+	 * Negative Scenario When I send a GET request to REST API URL
+	 * https://restful-booker.herokuapp.com/booking/1001
+	 * Then HTTP Status code
+	 * should be 404 And
+	 * Status Line should be HTTP/1.1 404 Not Found And Response
+	 * body contains "Not Found"
+	 * And Response body does not contain "TechProEd"
+	 */
+	@Test
+	public void get01() {
+		
+		//1.Step: Set URL
+		String url="https://restful-booker.herokuapp.com/booking/1001";
+		
+		//2.Step: Accepted date
+		
+		//3.Step: Send URL
+		Response response= given().accept(ContentType.JSON).when().get(url);
+		
+		response.prettyPrint();
+		
+		//4.Step: Make assertion
+		response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+	
+//		assertTrue(response.asString().contains("Not Found"));
+//		
+//		assertFalse(response.asString().contains("TechProEd"));
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+		softAssert.assertTrue(response.asString().contains("Not Found"));
+		System.out.println("=============");
+		softAssert.assertFalse(response.asString().contains("TechProEd"));
+		
+		softAssert.assertAll();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
